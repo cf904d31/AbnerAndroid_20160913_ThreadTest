cfe67d4a1d62e0f70a0a124cf98267ca9a7b283f
cf904d31 <cf904d31@gmail.com>
2016-09-13 09:16:03 +0800

    AbnerAndroid_ThreadTest

--- app/src/main/java/iii/org/tw/threadtest/MainActivity.java
+++ app/src/main/java/iii/org/tw/threadtest/MainActivity.java
@@ -2,12 +2,20 @@ package iii.org.tw.threadtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+    }
+
+    private void test1(View v) {
+
     }
 }
